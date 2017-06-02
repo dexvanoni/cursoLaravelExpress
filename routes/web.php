b9be0f17073bc48f2c35cccc41449e0297bbab9d
Denis Vanoni <dex.vanoni@gmail.com>
2017-06-01 20:06:05 -0400

    Término da seção 6 - CRUD e Rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@
 
 Route::get('/', 'PostsController@index');
 
-Route::get('admin/posts', ['as' => 'admin.posts.index', 'uses' =>'PostsAdminController@index']);
-Route::get('admin/posts/create', ['as' => 'admin.posts.create', 'uses' =>'PostsAdminController@create']);
-Route::post('admin/posts/store', ['as' => 'admin.posts.store', 'uses' =>'PostsAdminController@store']);
-Route::get('admin/posts/edit/{id}', ['as' => 'admin.posts.edit', 'uses' =>'PostsAdminController@edit']);
-Route::put('admin/posts/update/{id}', ['as' => 'admin.posts.update', 'uses' =>'PostsAdminController@update']);
-Route::get('admin/posts/destroy/{id}', ['as' => 'admin.posts.destroy', 'uses' =>'PostsAdminController@destroy']);
+Route::group(['prefix'=>'admin'], function() {
+  Route::group(['prefix'=>'posts'], function(){
+    Route::get('', ['as' => 'admin.posts.index', 'uses' =>'PostsAdminController@index']);
+    Route::get('create', ['as' => 'admin.posts.create', 'uses' =>'PostsAdminController@create']);
+    Route::post('store', ['as' => 'admin.posts.store', 'uses' =>'PostsAdminController@store']);
+    Route::get('edit/{id}', ['as' => 'admin.posts.edit', 'uses' =>'PostsAdminController@edit']);
+    Route::put('update/{id}', ['as' => 'admin.posts.update', 'uses' =>'PostsAdminController@update']);
+    Route::get('destroy/{id}', ['as' => 'admin.posts.destroy', 'uses' =>'PostsAdminController@destroy']);
+  });
+});
